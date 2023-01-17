8ae270846a3eeaf4bef0d711d26b287e8b1b46d4
Zwakeely9 <109646302+Zwakeely9@users.noreply.github.com>
2023-01-17 18:28:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function ageChecker(){
+    let age = prompt('What is your age?');
+    if (age <= 19){
+        alert('This person is underage! What are you doing here?!');
+    } else {
+        alert('This person can buy alcohol! Full speed ahead!');
+    }
+}
